4b1ea663fe508dce8fd8f174bd017d2dbc74f794
Karen van Eck <sners_nass@yahoo.co.uk>
2015-08-27 08:39:02 +0100

    start adding alarm stuff

--- ChoreRota/src/com/kve/chorerota/alarm/ChoreAlarm.java
+++ ChoreRota/src/com/kve/chorerota/alarm/ChoreAlarm.java
@@ -1,10 +1,13 @@
 package com.kve.chorerota.alarm;
 
+import java.util.Calendar;
 import java.util.Date;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 
+//	This one to set the alarm
 public class ChoreAlarm {
 
 	public ChoreAlarm()
@@ -14,7 +17,13 @@ public class ChoreAlarm {
 	
 	public void setAlarm(PendingIntent pendingIntent, Date schedTime, Context context)
 	{
-		
+		//	http://androidideasblog.blogspot.co.uk/2011/07/alarmmanager-and-notificationmanager.html
+		Calendar schedCal = Calendar.getInstance();
+		schedCal.setTime(schedTime);
+		long firstNotiTime = schedCal.getTimeInMillis();
+		AlarmManager am = 
+				(AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+		am.set(AlarmManager.RTC_WAKEUP, firstNotiTime, pendingIntent);
 	}
 
 }
---
+++ ChoreRota/src/com/kve/chorerota/alarm/ChoreAlarmReceiver.java
@@ -0,0 +1,16 @@
+package com.kve.chorerota.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ChoreAlarmReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		//	This has to receive an intent containing the notification
+		
+	}
+
+}
