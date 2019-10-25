2fa15f539a0fb11765d7c7d6770697d8590bcd0a
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2019-10-25 04:46:41 +0100

    binding 1

--- AboutSkania/app/src/main/java/com/sners/aboutskania/MainActivity.kt
+++ AboutSkania/app/src/main/java/com/sners/aboutskania/MainActivity.kt
@@ -9,26 +9,28 @@ import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.sners.aboutskania.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
-  //  private  lateinit var binding: ActivityMainBinding
+    private  lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-      //  binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-//        binding.doneButton.setOnClickListener {
-//            addNickname(it)
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
         }
     }
 
     private fun addNickname(view: View)
     {
-//        val viewEditText = findViewById<EditText>(R.id.nickname_edit)
-//        val nicknameText = findViewById<TextView>(R.id.nickname_text)
-//        binding.nicknameText.text = binding.nicknameEdit.text
-//        viewEditText.visibility = View.GONE
-//        binding.nicknameText.visibility = View.VISIBLE
+
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+
     }
 }
