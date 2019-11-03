6f7eab6265b1c1d4c5ffe6fd8d8442195fc2e63e
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2019-11-03 07:58:03 +0000

    first scruffy version

--- KarensTemperatureApp/app/src/main/java/com/sners/karenstemperatureapp/MainActivity.kt
+++ KarensTemperatureApp/app/src/main/java/com/sners/karenstemperatureapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +24,11 @@ class MainActivity : AppCompatActivity() {
         var fAlpha = 0.5f
         if (view.id == R.id.celcius_button) {
             cAlpha = 1.0f
+            this.convertToCelsius()
         }
         else if (view.id == R.id.fahrenheit_button) {
             fAlpha = 1.0f
+            this.convertToFahrenheit()
         }
 
         celciusButton.alpha = cAlpha
@@ -33,5 +37,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+  private fun convertToCelsius()
+    {
+        val textEdit = this.findViewById<EditText>(R.id.edit_temperature)
+        val fahrValue = textEdit.text.toString().toFloat()
+        val celValue = (fahrValue - 32 / 1.8)
+        val resultText = this.findViewById<TextView>(R.id.label_temperature)
+        resultText.text = "$celValue deg C"
+
+    }
+
+    private fun convertToFahrenheit()
+    {
+        val textEdit = this.findViewById<EditText>(R.id.edit_temperature)
+        val celsiusValue = textEdit.text.toString().toFloat()
+        val fahrValue = (celsiusValue * 1.8) + 32
+        val resultText = this.findViewById<TextView>(R.id.label_temperature)
+        resultText.text = "$fahrValue deg F"
+
+    }
 }
 
