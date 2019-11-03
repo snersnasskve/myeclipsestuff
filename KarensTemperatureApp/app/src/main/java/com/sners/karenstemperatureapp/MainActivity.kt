dc024aaa772de3d437f2127dcc0e71461b59198f
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2019-11-03 07:36:46 +0000

    button chanigng opacity

--- KarensTemperatureApp/app/src/main/java/com/sners/karenstemperatureapp/MainActivity.kt
+++ KarensTemperatureApp/app/src/main/java/com/sners/karenstemperatureapp/MainActivity.kt
@@ -11,5 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+    public fun buttonClicked(view : View)
+    {
+        val celciusButton = findViewById<Button>(R.id.celcius_button)
+        val fahrButton = findViewById<Button>(R.id.fahrenheit_button)
+        var cAlpha = 0.5f
+        var fAlpha = 0.5f
+        if (view.id == R.id.celcius_button) {
+            cAlpha = 1.0f
+        }
+        else if (view.id == R.id.fahrenheit_button) {
+            fAlpha = 1.0f
+        }
+
+        celciusButton.alpha = cAlpha
+        fahrButton.alpha = fAlpha
+
+
+    }
+
 }
 
