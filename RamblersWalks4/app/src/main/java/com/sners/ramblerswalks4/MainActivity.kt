40e416837e12356c342627fafaf2d6c2dc7f31b0
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2019-11-08 04:03:38 +0000

    Documenting the app lifecycle

--- RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/MainActivity.kt
+++ RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //  Happens once at the beginning
         setContentView(R.layout.activity_main)
         //val binding = DataBindingUtil.setContentView<>(this, R.layout.activity_main)
        // val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
@@ -27,21 +28,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        //  Activity gains focus
         Timber.i("onResume called from karen")
     }
 
     override fun onStart() {
         super.onStart()
+        //  Visible but not in focus
         Timber.i("onStart called from karen")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        //  Called instead of onStart if onStart has already been called once
+        Timber.i("onRestart called from karen")
+
     }
 
     override fun onPause() {
         super.onPause()
+        //  Activity loses focus
+        //  This blocks the UI so must be kept lightweight
         Timber.i("onPause called from karen")
     }
 
     override fun onStop() {
         super.onStop()
+        //  Removed from the screen - no longer visible
         Timber.i("onStop called from karen")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //  Happens once at the very end
+        Timber.i("onDestroy called from karen")
+
     }
 }
