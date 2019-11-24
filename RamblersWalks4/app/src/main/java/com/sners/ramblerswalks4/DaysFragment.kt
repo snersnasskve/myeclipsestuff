d51ac5039266fb727450910a8618802cd2a46ff3
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2019-11-24 05:54:06 +0000

    Observer from datamodel

--- RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/DaysFragment.kt
+++ RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/DaysFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.CompoundButton
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.sners.ramblerswalks4.data.DaysViewModel
 import timber.log.Timber
@@ -16,7 +18,7 @@ import timber.log.Timber
 /**
  * A simple [Fragment] subclass.
  */
-class DaysFragment : Fragment(), CompoundButton.OnCheckedChangeListener {
+class DaysFragment : Fragment(), CompoundButton.OnCheckedChangeListener, LifecycleObserver {
 
     lateinit var monday_checkbox : CheckBox
     lateinit var tuesday_checkbox : CheckBox
@@ -57,6 +59,50 @@ class DaysFragment : Fragment(), CompoundButton.OnCheckedChangeListener {
         this.weekends_checkbox = view.findViewById(R.id.weekdends_checkbox)
         this.everyday_checkbox = view.findViewById(R.id.everyday_checkbox)
 
+        this.monday_checkbox.setOnCheckedChangeListener(this)
+        this.tuesday_checkbox.setOnCheckedChangeListener(this)
+        this.wednesday_checkbox.setOnCheckedChangeListener(this)
+        this.thursday_checkbox.setOnCheckedChangeListener(this)
+        this.friday_checkbox.setOnCheckedChangeListener(this)
+        this.saturday_checkbox.setOnCheckedChangeListener(this)
+        this.sunday_checkbox.setOnCheckedChangeListener(this)
+        this.weekdays_checkbox.setOnCheckedChangeListener(this)
+        this.weekends_checkbox.setOnCheckedChangeListener(this)
+        this.everyday_checkbox.setOnCheckedChangeListener(this)
+
+        viewModel.monday.observe(this, Observer{newValue ->
+            monday_checkbox.isChecked = newValue
+        })
+        viewModel.tuesday.observe(this, Observer{newValue ->
+            tuesday_checkbox.isChecked = newValue
+        })
+        viewModel.wednesday.observe(this, Observer{newValue ->
+            wednesday_checkbox.isChecked = newValue
+        })
+        viewModel.thursday.observe(this, Observer{newValue ->
+            thursday_checkbox.isChecked = newValue
+        })
+        viewModel.friday.observe(this, Observer{newValue ->
+            friday_checkbox.isChecked = newValue
+        })
+        viewModel.saturday.observe(this, Observer{newValue ->
+            saturday_checkbox.isChecked = newValue
+        })
+        viewModel.sunday.observe(this, Observer{newValue ->
+            sunday_checkbox.isChecked = newValue
+        })
+        viewModel.weekdays.observe(this, Observer{newValue ->
+            weekdays_checkbox.isChecked = newValue
+        })
+        viewModel.weekend.observe(this, Observer{newValue ->
+            weekends_checkbox.isChecked = newValue
+        })
+        viewModel.everyday.observe(this, Observer{newValue ->
+            everyday_checkbox.isChecked = newValue
+        })
+
+        //this.updateValues()
+
         val backButton = view?.findViewById<Button>(R.id.days_back_button)
         backButton?.setOnClickListener {
             //  All fragments and activities have access to navigation
@@ -71,8 +117,12 @@ class DaysFragment : Fragment(), CompoundButton.OnCheckedChangeListener {
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
         Timber.i("tickbox changed from karen")
-        val tickedId = buttonView?.id
-
+        val tickedName = buttonView?.text
+        viewModel.tickBoxChanged(tickedName as String, isChecked)
+        //this.updateValues()
     }
+
+
+
 
 }
--- RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/data/DaysViewModel.kt
+++ RamblersWalks4/app/src/main/java/com/sners/ramblerswalks4/data/DaysViewModel.kt
@@ -1,28 +1,81 @@
 package com.sners.ramblerswalks4.data
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class DaysViewModel : ViewModel() {
 
-    var monday : Boolean = false
-    var tuesday : Boolean = false
-    var wednesday : Boolean = false
-    var thursday : Boolean = false
-    var friday : Boolean = false
-    var saturday : Boolean = false
-    var sunday : Boolean = false
-    var weekdays : Boolean = false
-    var weekend : Boolean = false
-    var everyday : Boolean = false
+    var monday = MutableLiveData<Boolean> ()
+    var tuesday = MutableLiveData<Boolean> ()
+    var wednesday = MutableLiveData<Boolean> ()
+    var thursday = MutableLiveData<Boolean> ()
+    var friday = MutableLiveData<Boolean> ()
+    var saturday = MutableLiveData<Boolean> ()
+    var sunday = MutableLiveData<Boolean> ()
+    var weekdays = MutableLiveData<Boolean> ()
+    var weekend = MutableLiveData<Boolean> ()
+    var everyday = MutableLiveData<Boolean> ()
 
     init {
         Timber.i("Days View Model created from karen")
-
+        monday.value = false
+        tuesday.value = false
+        wednesday.value = false
+        thursday.value = false
+        friday.value = false
+        saturday.value = false
+        sunday.value = false
+        weekdays.value = false
+        weekend.value = false
+        everyday.value = false
     }
 
     override fun onCleared() {
         super.onCleared()
         Timber.i("Days View Model destroyed from karen")
+    }
+
+    fun tickBoxChanged(name: String, checked: Boolean)
+    {
+        Timber.i("Received a tick event for $name of value $checked from karen")
+        when(name) {
+            "Monday" -> this.monday.value = checked
+            "Tuesday" -> this.tuesday.value = checked
+            "Wednesday" -> this.wednesday.value = checked
+            "Thursday" -> this.thursday.value = checked
+            "Friday" -> this.friday.value = checked
+            "Saturday" -> this.saturday.value = checked
+            "Sunday" -> this.sunday.value = checked
+            "Weekdays" -> this.weekdaysChanged(checked)
+            "Weekends" -> this.weekendsChanged(checked)
+            "Every Day" -> {
+                this.weekdaysChanged(checked)
+                this.weekendsChanged(checked)
+                this.everyday.value = checked
+            }
+            else -> Timber.i("Unknown tick event for $name from karen")
+        }
+        if (!checked){
+            this.everyday.value = checked
+            this.everyday.value = checked
+        }
+    }
+
+    fun weekdaysChanged(checked: Boolean)
+    {
+        this.weekdays.value = checked
+        this.monday.value = checked
+        this.tuesday.value = checked
+        this.wednesday.value = checked
+        this.thursday.value = checked
+        this.friday.value = checked
+    }
+
+    fun weekendsChanged(checked: Boolean)
+    {
+        this.weekend.value = checked
+        this.saturday.value = checked
+        this.sunday.value = checked
     }
 }
