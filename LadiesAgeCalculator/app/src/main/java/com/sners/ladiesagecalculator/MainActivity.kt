652d386084c584247ba22b3153c11fa2aa24d1ce
LAPTOP-Swifty\sners_nass <sners_nass@yahoo.co.uk>
2021-06-28 06:30:28 +0100

    add new LadiesAgeCalculator

---
+++ LadiesAgeCalculator/app/src/main/java/com/sners/ladiesagecalculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.sners.ladiesagecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * @property calculateButton The button to calculate your age
+     */
+    private var calculateButton: Button? = null
+
+    /**
+     * @property ageCalculated The resulting age text and description
+     */
+    private var ageCalculated: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        calculateButton = findViewById(R.id.calculate_button) as Button
+        ageCalculated = findViewById(R.id.age_calculated) as TextView
+
+        calculateButton!!.setOnClickListener {
+            ageCalculated!!.setText("You look fabulous")
+            Toast.makeText(this, "You pressed calc", Toast.LENGTH_LONG).show()
+        }
+    }
+}
